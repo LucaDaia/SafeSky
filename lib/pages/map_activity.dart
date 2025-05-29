e3d533683fae87353707024cfdce1a50a817fa6d
Luca Daia <87373703+LucaZeu@users.noreply.github.com>
2025-05-29 23:08:15 +0300

    Pages folder, Routes in main

--- lib/pages/map_activity.dart
+++ lib/pages/map_activity.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:safe_sky/utils/dark_map.dart';
+import 'package:top_snackbar_flutter/custom_snack_bar.dart';
+import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
 class MapActivity extends StatefulWidget {
   @override
@@ -8,44 +13,71 @@ class MapActivity extends StatefulWidget {
 
 class _MapActivityState extends State<MapActivity> {
 
-  void _onMapCreated(GoogleMapController controller) {
-    _mapController = controller;
+  //for zooming in on the location on activity start
+  late GoogleMapController _mapController;
+  LatLng? _currentPosition;
+
+  //for markers and reports
+  LatLng? _currentMarkedPosition;
+  Set<Marker> _markers = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _determinePosition();
   }
 
-  late GoogleMapController _mapController;
+  Future<void> _determinePosition() async {
+    // Request permission
+    await Permission.location.request();
 
-  // Center of the initial camera position
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+    // Check permission status
+    if (await Permission.location.isGranted) {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
 
+      setState(() {
+        _currentPosition = LatLng(position.latitude, position.longitude);
+      });
+
+      _mapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: _currentPosition!, //  ! for promising its not a null value
+            zoom: 16.0,
+          ),
+        ),
+      );
+    } else {
+      // Handle permission denied
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Location permission not granted')),
+      );
+    }
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    _mapController = controller;
+    _mapController.setMapStyle(DarkMap.darkMapStyle);
+  }
+// TODO: Add a drawer here
+  // TODO: Maybe remove the AppBar - looks ugly
+  // TODO: add a light theme
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text(
-            'SafeSky',
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold
-            ),
-          ),
-        ),
-        backgroundColor: Colors.blueGrey,
-      ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 11.0,
-        ),
-        mapType: MapType.normal,
-        myLocationButtonEnabled: true,
-        myLocationEnabled: true,
-      ),
       floatingActionButton: FloatingActionButton.large(
         onPressed: () {
-          print("A fost raportat un pericol");
+          if(_currentMarkedPosition != null) {
+            Navigator.pushNamed(context, '/report',
+                arguments: {'reportedPosition': _currentMarkedPosition});
+          }
+          else {
+            showTopSnackBar(
+                Overlay.of(context),
+                CustomSnackBar.error(message: "Must choose a location first!"));
+          }
         },
         child: Icon(
           Icons.report_gmailerrorred,
@@ -54,7 +86,44 @@ class _MapActivityState extends State<MapActivity> {
         backgroundColor: Colors.red[900],
         shape: CircleBorder(),
         elevation: 50,
+
       ),
-    );
-  }
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _currentPosition ?? LatLng(45.521563, -122.677433), // fallback
+          zoom: 11.0,
+        ),
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        markers: _markers,
+        onLongPress: (LatLng pos) {
+
+          setState(() {
+            _currentMarkedPosition = pos;
+            _markers = {
+              Marker(
+                markerId: MarkerId(pos.toString()),
+                position: pos,
+                onTap: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Danger Zone at (${pos.latitude}, ${pos.longitude})'),
+                      duration: Duration(seconds: 3),
+                    ),
+                  );
+                },
+              ),
+            };
+
+          });
+
+         showTopSnackBar(
+             Overlay.of(context),
+             CustomSnackBar.info(message: 'Selected A Danger Zone!'));
+    },
+    ),
+
+    );}
 }
--- lib/pages/report_danger.dart
+++ lib/pages/report_danger.dart
@@ -1,13 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class ReportDanger extends StatelessWidget {
+class ReportDanger extends StatefulWidget {
+  const ReportDanger({super.key});
+
+  @override
+  State<ReportDanger> createState() => _ReportDangerState();
+}
+
+class _ReportDangerState extends State<ReportDanger> {
+  final _formKey = GlobalKey<FormState>();
+  String? _selectedGravity;
+  String? _details;
+
+
   @override
   Widget build(BuildContext context) {
+
+    final args = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+    final LatLng reportedPosition = args['reportedPosition'];
+
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Text('Report danger screen'),
+      appBar: AppBar(
+        title: Text(
+          'Report Danger',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ) ,),
+        backgroundColor: Colors.black,
       ),
+      body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+              key: _formKey, //what is formKey
+              child: Column(
+                children: [
+                  DropdownButtonFormField<String>(
+                    value: _selectedGravity,
+                    decoration: InputDecoration(
+                      labelText: "Danger Level",
+                      border: OutlineInputBorder(),
+                    ),
+                    items: ['Low', 'Medium', 'High']
+                        .map((level) => DropdownMenuItem(
+                      value: level,
+                      child: Text(level),
+                    ))
+                        .toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedGravity = value;
+                      });
+                    },
+                    validator: (value) => value == null ? 'Please select a danger level' : null,
+                  ),
+                  SizedBox(height: 20,),
+                  TextFormField(
+                    maxLines: 5,
+                    decoration: InputDecoration(
+                      labelText: 'Details',
+                      border: OutlineInputBorder(),
+                      alignLabelWithHint: true,
+                    ),
+                    onSaved: (value) => _details = value,
+                    validator: (value) =>
+                    value == null || value.isEmpty ? 'Please enter details' : null,
+                  ),
+                  SizedBox(height: 20,),
+                  ElevatedButton(
+                    onPressed: () {
+                      if(_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        DateTime now = DateTime.now();
+
+                        //TODO: Send this to Firebase
+                        print('Gravity: $_selectedGravity');
+                        print('Details: $_details');
+                        print('Position: ${reportedPosition.latitude}, ${reportedPosition.longitude}');
+                        print('Time: $now');
+
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Report submitted!')),
+                        );
+
+
+                        //TODO: send back a response code true
+                        Navigator.pop(context);
+                      }
+                    },
+                    child: Text('Submit Report'),
+                  )
+
+                ],
+              ))
+      ),
+      backgroundColor: Colors.grey[800],
     );
   }
 }
+
+
+
